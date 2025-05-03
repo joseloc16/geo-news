daee69c15bb2843bd25d91cafa7580164c228968
José Nuñez Morocho <darksoul.js17@gmail.com>
2025-05-03 10:24:51 -0500

    first commit

---
+++ src/main/java/com/joseloc/geo_news/GeoNewsApplication.java
@@ -0,0 +1,13 @@
+package com.joseloc.geo_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseloc/geo_news/GeoNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseloc.geo_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
